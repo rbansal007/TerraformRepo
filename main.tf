e2536ef6ae4a2420b27afb9d87d7f53935f5ce55
Ramit Bansal <ramit.bansal@hashicorp.com>
2026-02-01 19:40:28 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.8.1"
+    }
+  }
+}
+
+provider "random" {
+  # Configuration options
+}
+
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}

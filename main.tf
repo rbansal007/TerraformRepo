56ee0e171303ebda45e013661f253154ee7f242f
Ramit Bansal <ramit.bansal@hashicorp.com>
2026-09-10 12:46:46 +0530

    Change password length from 21 to 20

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "random" {
 }
 
 resource "random_password" "password" {
-  length           = 21
+  length           = 20
   special          = true
   override_special = "!#$%&*()-_=+[]{}<>:?"
 }
